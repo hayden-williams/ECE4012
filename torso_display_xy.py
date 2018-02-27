e7bdcc4cf6c5af2fa88600b3066ea9fa5554703d
hayden-williams <hwilliams123456789@gmail.com>
2018-02-27 16:05:17 -0500

    get x,y pos for skeleton_markers

---
+++ torso_display_xy.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python  
+    import roslib
+    import rospy
+    import math
+    import tf
+    import geometry_msgs.msg
+    
+   if __name__ == '__main__':
+       rospy.init_node('turtle_tf_listener')
+   
+       listener = tf.TransformListener()
+   
+   
+   
+       rate = rospy.Rate(10.0)
+       while not rospy.is_shutdown():
+           try:
+               (trans,rot) = listener.lookupTransform('/torso_2', '/openni_depth_frame', rospy.Time(0))
+           except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+               continue
+   
+
+   
+           rate.sleep()
