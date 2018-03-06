a4f0d39f6d4392eb301eaad20898e775e9593cc5
emarchand3 <emarchand3@gatech.edu>
2018-03-06 15:37:01 -0500

    mispelled point

--- torso_display_xy.py
+++ torso_display_xy.py
@@ -67,7 +67,7 @@ class GoForward():
       
   def skeletonMarkers(self,data):
     print "Hello from skeletonMarkers"
-    vec = Marker.point[3]
+    vec = Marker.points[3]
     vec2 = [2,3]
     rospy.loginfo("Size %f , Value %f. TestShape: %f"%(vec.shape, vec, vec2.shape))      
     
