f1fef66f5fc3cd5671967d66db5a5ef8b0561cec
emarchand3 <emarchand3@gatech.edu>
2018-03-08 15:23:52 -0500

    .

--- skeleton_tf.py
+++ skeleton_tf.py
@@ -4,13 +4,16 @@ import tf
 import geometry_msgs.msg
 
 if __name__ == '__main__':
+
+  num = 2
   rospy.init_node('listener_skeleton', anonymous=False)
   listener = tf.TransformListener()
 
   rate = rospy.Rate(10)
   while not rospy.is_shutdown():
+    torso = "torso_" + str(num)
     try:
-      (trans,rot) = listener.lookupTransform ('/openni_depth_frame', 'torso_2', rospy.Time(0))
+      (trans,rot) = listener.lookupTransform ('/openni_depth_frame', torso, rospy.Time(0))
     except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
       continue
 
