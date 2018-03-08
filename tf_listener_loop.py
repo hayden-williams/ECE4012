c02a68218b0cfa27ef6c21b29d0d310021aeb866
emarchand3 <emarchand3@gatech.edu>
2018-03-08 15:29:48 -0500

    .

--- tf_listener_loop.py
+++ tf_listener_loop.py
@@ -11,16 +11,16 @@ if __name__ == '__main__':
 
   rate = rospy.Rate(10)
   while not rospy.is_shutdown():
-    torso = 'torso_' + str(self.num)
+    torso = 'torso_' + str(num)
     print torso
     try:
-      (trans,rot) = self.listener.lookupTransform('/openni_depth_frame', torso, rospy.Time(0) )
+      (trans,rot) = listener.lookupTransform('/openni_depth_frame', torso, rospy.Time(0) )
 
       print trans
     except:
-      self.num = self.num + 1
-      if self.num == 10:
-        self.num = 1
+      num = num + 1
+      if num == 10:
+        num = 1
       print 'Looking for user'
       continue
 
