ae497df36cd77bca4cea57fdfbb434012bd79e97
emarchand3 <emarchand3@gatech.edu>
2018-03-13 19:58:35 -0400

    .

--- User_Following_V2.py
+++ User_Following_V2.py
@@ -58,7 +58,7 @@ class image_converter:
 			self.mask = cv2.inRange(self.depth_image, min_z, max_z)
 			image = cv2.bitwise_and(self.depth_image,self.depth_image, mask= self.mask)
 			#image = cv2.bitwise_and(image,image, mask= self.mask2)
-			rospy.loginfo(str(self.mask[cR,cC]))
+			rospy.loginfo(str(np.array(4,dtype="uint8)")))
 
 
 				#cv2.imshow('image',image)
