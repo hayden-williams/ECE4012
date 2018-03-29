bda76e2ac893f32da9343e99e18e6975d1f2094f
emarchand3 <emarchand3@gatech.edu>
2018-03-29 16:48:52 -0400

    Add files via upload

--- User_Following_V2.py
+++ User_Following_V2.py
@@ -103,12 +103,12 @@ class image_converter:
 				cx = int(M['m10']/M['m00'])
 				cy = int(M['m01']/M['m00'])
 				centerOfObject = (int(cx),int(cy))
-				rospy.loginfo(centerOfObject)
+				#rospy.loginfo(centerOfObject)
 				#cv2.circle(image,centerOfObject,10,(0,255,0),-1)
 				#rospy.loginfo("in if statement in callback")
 				dx = cx - col/2 # +ve move left, -ve move right?
 				dy = cy - rows/2
-				#rospy.loginfo(dx)
+				rospy.loginfo(dx)
 
 				#Movement code to center object and keep desired distance
 					#self.move_cmd.linear.x = 0.0015*(-1)*dy
