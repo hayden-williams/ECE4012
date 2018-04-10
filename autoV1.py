07aa30b1ed78e64c67b671ed3fac6ab4cbdb8131
emarchand3 <emarchand3@gatech.edu>
2018-04-10 17:36:10 -0400

    Add files via upload

--- autoV1.py
+++ autoV1.py
@@ -91,11 +91,14 @@ class GoStraight():
 			else:
 				rospy.loginfo("inside else")
 				if (self.ZoneList[3] != 0 ):
+					rospy.loginfo("inside if")
 					while (self.ZoneList[3] != 0):
+						rospy.loginfo("inside while")
 						self.move_cmd.angular.z = .1
 						self.move_cmd.linear.x = .2
 						self.cmd_vel.publish(move_cmd)
 						self.r.sleep()
+					rospy.loginfo("outside while")
 					self.move_cmd.angular.z =self.kTurn*self.thetaError
 					self.move_cmd.linear.x = .2
 					self.cmd_vel.publish(move_cmd)
