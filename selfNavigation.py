8ef29d6c16b4ba8c2c3ec976e46232507152173c
hayden-williams <hwilliams123456789@gmail.com>
2018-04-15 17:09:59 -0400

    merged code with obstacle avoidance

--- selfNavigation.py
+++ selfNavigation.py
@@ -28,6 +28,12 @@ import time
 import json
 #from tf2_msgs.msg import TFMessage
 #import tf
+import sys 
+import cv2
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
 
 class selfNavigation():
 	thetaError = 0
@@ -36,7 +42,7 @@ class selfNavigation():
 	direction = 1000
 	bearing = 1000
 	length = 0
-	count = 0
+	countQuery = 0
 
 	odomBearing = 0
 	zeroAngle = 1000
@@ -45,6 +51,11 @@ class selfNavigation():
 	xstart = 0
 	ystart = 0
 	magnitude = 9999999.0
+
+	z_thresh = 1000
+	z_threshCorner = z_thresh
+	ZoneList = np.array([0,0,0,0,0,0])
+	count = 0
 
 	def __init__(self):
 		# initiliaze
@@ -61,6 +72,11 @@ class selfNavigation():
 		rospy.Subscriber('odom',Odometry,self.Orientation)
 
 		# may need rospy.spin(); 
+
+		# Initialize bridge
+		self.bridge = CvBridge()
+		# Subscribe to depth sensor and get raw image
+		self.depth_sub = rospy.Subscriber("/camera/depth/image_raw",Image,self.callback)
 
 		
 		# Create a publisher which can "talk" to TurtleBot and tell it to move
@@ -82,7 +98,7 @@ class selfNavigation():
 		while not rospy.is_shutdown():
 
 			# get info from server
-			if self.count%1000 == 0:
+			if self.countQuery%1000 == 0:
 				#only check server occationally
 				r = requests.get('http://128.61.7.199:3000/rover').json()
 				rospy.loginfo(r)
@@ -97,7 +113,7 @@ class selfNavigation():
 				goToUser = r['goToUser']
 
 
-			self.count = self.count + 1
+			self.countQuery = self.countQuery + 1
 
 			if goToUser==1:
 				# put navigation code here
@@ -107,36 +123,90 @@ class selfNavigation():
 				self.desiredAngle = (360-self.direction)*3.14159265359/180 # input degree convert to rad
 				# using odometry for bearing
 				# IndoorAtlus East is 90, Odometry West is 90, Need to account for this
-				"""
-				if self.direction != 1000:
-					print('calc error')
-					if (self.bearing>180):
-						self.bearing = self.bearing - 360
-					if self.direction > 180:
-						self.direction = self.direction - 360
-					#self.thetaError = (-1)*(self.direction - self.bearing) # -ve = turn right, +ve = turn left 
-				"""
+				
+				
+
+				rospy.loginfo(np.absolute(self.thetaError))
+				if (np.absolute(self.thetaError) < 0.25):
+					self.count = 0
+			
+				rospy.loginfo("obstacle " + str(self.ZoneList))
+				if (np.sum(self.ZoneList) == 0):
+
+					#no obstacle, move code goes here
+					rospy.loginfo("after desiredAngle")
+					rospy.loginfo(self.magnitude)
+					rospy.loginfo(self.length)
+					if (self.direction == 1000 or self.length == 0.0 or self.zeroAngle == 1000 or self.magnitude == 9999999.0):
+						rospy.loginfo('len = 0, dir = 1000')
+						move_cmd.linear.x = 0.0
+						move_cmd.angular.z = 0
+					elif (fabs(self.thetaError) < 1 and self.magnitude < self.length):
+						rospy.loginfo('error < 0.05')
+						move_cmd.angular.z = self.kTurn*self.thetaError
+						move_cmd.linear.x = 0.2
+					elif fabs(self.thetaError) > 1:
+						rospy.loginfo('error>0.05')
+						move_cmd.angular.z = self.kTurn*self.thetaError
+						move_cmd.linear.x = 0.0
+					else:
+						rospy.loginfo('else')
+						move_cmd.angular.z = self.kTurn*self.thetaError
+						move_cmd.linear.x = 0.0
 
 
-				rospy.loginfo("after desiredAngle")
-				rospy.loginfo(self.magnitude)
-				rospy.loginfo(self.length)
-				if (self.direction == 1000 or self.length == 0.0 or self.zeroAngle == 1000 or self.magnitude == 9999999.0):
-					rospy.loginfo('len = 0, dir = 1000')
-					move_cmd.linear.x = 0.0
-					move_cmd.angular.z = 0
-				elif (fabs(self.thetaError) < 1 and self.magnitude < self.length):
-					rospy.loginfo('error < 0.05')
-					move_cmd.angular.z = self.kTurn*self.thetaError
-					move_cmd.linear.x = 0.2
-				elif fabs(self.thetaError) > 1:
-					rospy.loginfo('error>0.05')
-					move_cmd.angular.z = self.kTurn*self.thetaError
-					move_cmd.linear.x = 0.0
 				else:
-					rospy.loginfo('else')
-					move_cmd.angular.z = self.kTurn*self.thetaError
-					move_cmd.linear.x = 0.0
+					if (self.ZoneList[0] == 0 and self.ZoneList[1] == 0 and self.ZoneList[2] == 0 and self.ZoneList[3] != 0):
+						#rospy.loginfo("inside else")
+						#soft left
+						move_cmd.linear.x = 0.2
+						move_cmd.angular.z = 0.5
+					elif (self.ZoneList[0] != 0 and self.ZoneList[1] == 0 and self.ZoneList[2] == 0 and self.ZoneList[3] == 0):
+						#soft right
+						move_cmd.linear.x = 0.2
+						move_cmd.angular.z = -0.5
+					elif (self.ZoneList[0] == 0 and self.ZoneList[1] != 0 and self.ZoneList[2] != 0 and self.ZoneList[3] == 0):
+						if (self.ZoneList[1] > self.ZoneList[2]):
+							#Hard Right
+							move_cmd.linear.x = 0.2
+							move_cmd.angular.z = -0.75
+						else:
+							move_cmd.linear.x = 0.2
+							move_cmd.angular.z = 0.7
+							#Hard Leff
+					elif((self.ZoneList[0]==0 and self.ZoneList[2] !=0) or (self.ZoneList[0] == 0 and self.ZoneList[1] !=0 and self.ZoneList[3] != 0)):
+						move_cmd.linear.x = 0.2
+						move_cmd.angular.z = 0.7
+					# Hard Left
+					elif((self.ZoneList[1] != 0 and self.ZoneList[3] ==0) or (self.ZoneList[0] != 0 and self.ZoneList[2] != 0 and self.ZoneList[3] == 0)):
+						move_cmd.linear.x = 0.2
+						move_cmd.angular.z = -0.75
+					else:
+						self.count = self.count + 1
+						if self.count == 1 :
+							move_cmd.linear.x = 0.0
+							move_cmd.angular.z = 0
+							self.cmd_vel.publish(move_cmd)
+							self.r.sleep()
+							while (np.sum(self.ZoneList) != 0 and np.absolute(self.thetaError) < 1.57):
+								move_cmd.angular.z = 0.5
+								self.cmd_vel.publish(move_cmd)
+								self.r.sleep()
+						elif self.count == 2 :
+							move_cmd.linear.x = 0.0
+							move_cmd.angular.z = 0
+							self.cmd_vel.publish(move_cmd)
+							self.r.sleep()
+							while (np.sum(self.ZoneList)!=0):
+								move_cmd.angular.z = -0.5
+								self.cmd_vel.publish(move_cmd)
+								self.r.sleep()
+						else:
+							rospy.loginfo("I cant make it around! Help Mommy")
+							move_cmd.linear.x = 0.0
+							move_cmd.angular.z = 0
+							self.cmd_vel.publish(move_cmd)
+
 
 				# publish the velocity
 				rospy.loginfo('publish')
@@ -171,6 +241,89 @@ class selfNavigation():
 		y = data.pose.pose.position.y - self.ystart
 		self.magnitude = sqrt(x**2 + y**2)
 
+	def callback(self,data):
+		try:
+			
+
+
+			# Get Image and find size of image
+			self.depth_image = self.bridge.imgmsg_to_cv2(data, "passthrough")
+			rows, col, channels = self.depth_image.shape #grey scale channel is 1, rgb is 3
+
+			# Find Center of Image
+			cR = np.int(np.round(rows/2))
+			cC = np.int(np.round(col/2))
+
+
+			colFrac = np.int(np.round(.25*col))
+
+
+			self.mask =  np.zeros((rows,col))
+			self.mask[cR-rows*.25:rows,0:col] = 5
+			self.mask = np.uint16(self.mask)
+			self.mask = cv2.inRange(self.mask,np.array(4,dtype = "uint16"),np.array(6,dtype = "uint16"))
+
+
+			min_z= np.array(100, dtype = "uint16") #bgr
+			max_z= np.array(self.z_thresh, dtype = "uint16")
+			self.mask2 = cv2.inRange(self.depth_image, min_z, max_z)
+			
+			#Combination of masks
+			self.mask3 = cv2.bitwise_and(self.mask,self.mask, mask= self.mask2)
+
+			min_zCorn= np.array(100, dtype = "uint16") #bgr
+			max_zCorn= np.array(self.z_threshCorner, dtype = "uint16")
+			self.maskCorner = cv2.inRange(self.depth_image, min_zCorn, max_zCorn)
+
+			self.maskZone1 = np.zeros((rows,col))
+			self.maskZone1[0:rows,0:np.round(col/4)] = 5
+			self.maskZone1 = np.uint16(self.maskZone1)
+			self.maskZone1 = cv2.inRange(self.maskZone1,np.array(4,dtype = "uint16"),np.array(6,dtype = "uint16"))
+			self.Zone1 = cv2.bitwise_and(self.maskCorner,self.maskCorner, mask= self.maskZone1)
+
+			self.maskZone2 = np.zeros((rows,col))
+			self.maskZone2[0:rows,np.round(col/4)+1:np.round(col/2)] = 5
+			self.maskZone2 = np.uint16(self.maskZone2)
+			self.maskZone2 = cv2.inRange(self.maskZone2,np.array(4,dtype = "uint16"),np.array(6,dtype = "uint16"))
+			self.Zone2 = cv2.bitwise_and(self.mask3,self.mask3, mask= self.maskZone2)
+
+			self.maskZone3 = np.zeros((rows,col))
+			self.maskZone3[0:rows,np.round(col/2)+1:cC+np.round(col/4)] = 5
+			self.maskZone3 = np.uint16(self.maskZone3)
+			self.maskZone3 = cv2.inRange(self.maskZone3,np.array(4,dtype = "uint16"),np.array(6,dtype = "uint16"))
+			self.Zone3 = cv2.bitwise_and(self.mask3,self.mask3, mask= self.maskZone3)
+
+			self.maskZone4 = np.zeros((rows,col))
+			self.maskZone4[0:rows,cC+np.round(col/4)+1:col] = 5
+			self.maskZone4 = np.uint16(self.maskZone4)
+			self.maskZone4 = cv2.inRange(self.maskZone4,np.array(4,dtype = "uint16"),np.array(6,dtype = "uint16"))
+			self.Zone4 = cv2.bitwise_and(self.mask3,self.mask3, mask= self.maskZone4)
+
+
+			sumZone1 = np.sum(self.Zone1 / 255)
+			#rospy.loginfo("sum of Zone1 is " + str(sumZone1))
+			sumZone2 = np.sum(self.Zone2 / 255)
+			#rospy.loginfo("sum of Zone2 is " + str(sumZone2))
+			sumZone3 = np.sum(self.Zone3 / 255)
+			#rospy.loginfo("sum of Zone3 is " + str(sumZone3))
+			sumZone4 = np.sum(self.Zone4 / 255)
+			#rospy.loginfo("sum of Zone4 is " + str(sumZone4))
+
+
+			self.ZoneList = np.array([sumZone1, sumZone2, sumZone3, sumZone4])
+			#rospy.loginfo("Zone List is "+ str(self.ZoneList))
+
+
+
+
+				
+
+
+			self.r.sleep()
+
+
+		except CvBridgeError, e:
+			print e
 
 	def shutdown(self):
 		# stop turtlebot
