76aa68fbbca2dbfc6273bdbe1badb32a032ddaea
hayden-williams <hwilliams123456789@gmail.com>
2018-04-16 14:31:47 -0400

    .

--- emergencySoundTest.py
+++ emergencySoundTest.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
 			# Adjust the number so this says Emergency repeatedly
 			soundCounter = 0
 			soundhandle.say('Emergency')
-			soundhandle.stopSaying()
+			soundhandle.stopSaying(self, 'Emergency')
 		soundCounter = soundCounter + 1
 
 	print('While loop ended')
