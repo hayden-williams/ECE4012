2764d279988af05424a08377e26e1dd286692b3b
hayden-williams <hwilliams123456789@gmail.com>
2018-04-16 14:06:40 -0400

    added main

--- emergencySoundTest.py
+++ emergencySoundTest.py
@@ -9,19 +9,22 @@ import roslib
 import sys
 from sound_play.libsoundplay import SoundClient
 
-counter = 0
-soundCounter = 0
 
-rospy.init_node('emergency', anonymous=False)
+if __name__ == '__main__'
+	counter = 0
+	soundCounter = 0
 
-soundhandle = SoundClient()
+	rospy.init_node('emergency', anonymous=False)
+	rate = rospy.Rate(10)
 
-while(counter < 999999):
-	counter = counter + 1
-	if soundCounter%1000 == 0:
-		# Adjust the number so this says Emergency repeatedly
-		soundCounter = 0
-		soundhandle.say('Emergency')
-	soundCounter = soundCounter + 1
+	soundhandle = SoundClient()
 
-print('While loop ended')
+	while(counter < 999999):
+		counter = counter + 1
+		if soundCounter%1000 == 0:
+			# Adjust the number so this says Emergency repeatedly
+			soundCounter = 0
+			soundhandle.say('Emergency')
+		soundCounter = soundCounter + 1
+
+	print('While loop ended')
