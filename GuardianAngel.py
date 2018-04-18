00d32af3b89c9c81a6f59f3f6ffdd43ebe5b1e9e
emarchand3 <emarchand3@gatech.edu>
2018-04-18 11:08:20 -0400

    Add files via upload

--- GuardianAngel.py
+++ GuardianAngel.py
@@ -223,21 +223,21 @@ class following_final2():
 					elif((self.ZoneList[1] != 0 and self.ZoneList[3] ==0) or (self.ZoneList[0] != 0 and self.ZoneList[2] != 0 and self.ZoneList[3] == 0)):
 						self.move_cmd.linear.x = 0.2
 						self.move_cmd.angular.z = -0.75
-						
+					"""	
 					else:
 						rospy.loginfo('situation')
 						self.move_cmd.angular.z = 0.0
 						self.move_cmd.linear.x = 0.0
-						"""
-
-					elif (self.ZoneList[0] != 0 and self.ZoneList[3] != 0 and self.count<1):
+						
+					"""
+					elif (self.ZoneList[0] != 0 and self.ZoneList[1] != 0 and self.ZoneList[2] != 0 and self.ZoneList[3] != 0 and self.count<1):
 						while (np.absolute(self.thetaError) < 1.57):
 							self.move_cmd.angular.z = 0.3
 							self.move_cmd.linear.x = 0
 							self.cmd_vel.publish(self.move_cmd)
 							self.r.sleep()
 						self.count = self.count + 1
-					elif (self.ZoneList[0] != 0 and self.ZoneList[3] != 0 and self.count<2):
+					elif (self.ZoneList[0] != 0 and self.ZoneList[1] != 0 and self.ZoneList[2] != 0 and self.ZoneList[3] != 0 and self.count<2):
 						while (np.absolute(self.thetaError) < 1.57):
 							self.move_cmd.angular.z = -0.3
 							self.move_cmd.linear.x = 0
@@ -248,6 +248,9 @@ class following_final2():
 						rospy.loginfo("Im stuck")
 						self.move_cmd.angular.z = 0.0
 						self.move_cmd.linear.x = 0.0
+					else:
+						self.move_cmd.linear.x = 0.0
+						self.move_cmd.angular.z = 0
 						"""
 
 
